57f29a7d1f9bde46f12f57d48df753a02d4fc7c4
EC2 Default User <ec2-user@ip-172-31-59-153.ec2.internal>
2022-03-30 18:09:43 +0000

    Tested multiple subclass Giraffe objects

--- Zoo.java
+++ Zoo.java
@@ -6,6 +6,9 @@ public class Zoo
     {
         Species mySpecies = new Species("Mickey", 93, false);
         Giraffe motherGiraffe = new Giraffe("Kenya", 11, 160, 2500);
+        Giraffe daughterOne = new Giraffe("Amira", 4, 150, 2301.75);
+        Giraffe daughterTwo = new Giraffe("Mara", 1, 96, 1815.25);
+        
         
         /**
          * With the giraffe, we have access to methods from the 
@@ -16,10 +19,23 @@ public class Zoo
         System.out.println("Giraffe's height: " + motherGiraffe.getHeightInInches() + " inches");
         System.out.println("Giraffe's weight: " + motherGiraffe.getWeight() + " pounds");
         System.out.println("Is endangered?: " + motherGiraffe.isEndangered());
+        System.out.println(" =================================================================== ");
+        
+        System.out.println(daughterOne);
+        System.out.println("Giraffe's height: " + daughterOne.getHeightInInches() + " inches");
+        System.out.println("Giraffe's weight: " + daughterOne.getWeight() + " pounds");
+        System.out.println("Is endangered?: " + daughterOne.isEndangered());
+        System.out.println(" =================================================================== ");
+        
+        System.out.println(daughterTwo);
+        System.out.println("Giraffe's height: " + daughterTwo.getHeightInInches() + " inches");
+        System.out.println("Giraffe's weight: " + daughterTwo.getWeight() + " pounds");
+        System.out.println("Is endangered?: " + daughterTwo.isEndangered());
+        System.out.println(" =================================================================== ");
         
         /**
-         * With the person, we only have access to methods from 
-         * the Person class.
+         * With the species, we only have access to methods from 
+         * the Species class.
          */
          
         System.out.println("Species Name: " + mySpecies.getName());
